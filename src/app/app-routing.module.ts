d61d8339f822bbd55c6abd1fcad08db286b45be4
Oluwaseun <oluwaseunazure@yahoo.com>
2020-06-16 07:32:04 +0100

    login construction

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './admin/login/login.component';
+import {EmployeesComponent} from './employees/employees.component';
 
 
 const routes: Routes = [
-  {path: 'login', component: LoginComponent}
+  {path: '', component: LoginComponent},
+  {path: 'employees', component: EmployeesComponent},
+
+  {path: '**', redirectTo: ''}
 ];
 
 @NgModule({
